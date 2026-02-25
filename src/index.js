31acb57924489c03fd03918fc2a92913768bde09
Tesa Firna Ananta <h1101241057@student.untan.ac.id>
2026-02-25 04:54:31 +0000

    ya

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// src/index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(name, umur) {
+    return `selamat datang ` + name + ` dikelas pak syahru, sekarang umur kamu sudah` + umur + ` ya`;
+}
+const namaMahasiswa = "tesa";
+const umur = 19;
+console.log(hello(namaMahasiswa, umur));
+//# sourceMappingURL=index.js.map
